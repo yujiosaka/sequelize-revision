0fd216c99d5126957e20feb393af796835990c87
yujiosaka <yujisobe@gmail.comm>
2022-04-12 04:14:20 +0900

    ci: automate changelog by semantic release

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.export = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["docs/CHANGELOG.md"],
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
