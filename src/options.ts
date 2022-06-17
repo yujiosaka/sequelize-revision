e9e94e2f2ff9a3043d51f0e612f44941b3b969b5
yujiosaka <yujisobe@gmail.comm>
2022-06-18 00:04:40 +0900

    refactor: set options type to default options

--- src/options.ts
+++ src/options.ts
@@ -24,7 +24,7 @@ export interface Options {
   belongsToUserOptions?: BelongsToOptions;
 }
 
-export const defaultOptions = {
+export const defaultOptions: Options = {
   exclude: [
     "id",
     "createdAt",
